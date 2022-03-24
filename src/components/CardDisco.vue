1f52c7f9d7dbcc62af5ca8dd1ae277151cbdd108
Barbarubia <dnl.desimone@gmail.com>
2022-03-24 15:53:17 +0100

    creata card dischi e import dati da api con axios

---
+++ src/components/CardDisco.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img :src="discoData.poster" :alt="discoData.title">
+    <h3>{{ discoData.title }}</h3>
+    <span>{{ discoData.author }}</span>
+    <span>{{ discoData.year }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardDisco',
+  props: {
+    discoData: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/MainDischi.vue
+++ src/components/MainDischi.vue
@@ -1,12 +1,39 @@
 <template>
-  <main class="bg-primary">
-
+  <main class="bg-primary py-5">
+    <div class="container-sm container-md container-xl">
+      <div class="row">
+        <card-disco
+          v-for="disco in arrDischi"
+          :key="disco.title"
+          :disco-data="disco"
+        />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import CardDisco from './CardDisco.vue'
+import axios from 'axios'
+
 export default {
-  name: 'MainDischi'
+  name: 'MainDischi',
+  data () {
+    return {
+      arrDischi: null
+    }
+  },
+  components: {
+    CardDisco
+  },
+  created () {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((risposta) => {
+        // console.log(risposta)
+        this.arrDischi = risposta.data.response
+        console.log(this.arrDischi)
+      })
+  }
 }
 </script>
 
