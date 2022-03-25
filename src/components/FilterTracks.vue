b70e1ddf3d6d771419f121948418a912f1b1fd23
Barbarubia <dnl.desimone@gmail.com>
2022-03-25 15:30:43 +0100

    console.log del genere selezionato

--- src/components/FilterTracks.vue
+++ src/components/FilterTracks.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <select name="filter-genre" id="filter-genre">
-      <option value="all">Seleziona un genere</option>
-      <option value="rock">Rock</option>
-      <option value="pop">Pop</option>
-      <option value="jazz">Jazz</option>
-      <option value="metal">Metal</option>
+    <select @change="filter($event)" name="filter-genre" id="filter-genre">
+      <option value="">Seleziona un genere</option>
+      <option value="Rock">Rock</option>
+      <option value="Pop">Pop</option>
+      <option value="Jazz">Jazz</option>
+      <option value="Metal">Metal</option>
     </select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FilterTracks'
+  name: 'FilterTracks',
+  methods: {
+    filter (event) {
+      console.log(event.target.value)
+    }
+  }
 }
 </script>
 
